678af42b477b73210d51fd471acf9fdcd5df4ec8
Donovan Isherwood <donovan@matacore.co.za>
2016-12-04 23:52:06 +0100

    Initial commit of the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ProductsService } from './services/products-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  products = [];
+  cart = [];
+  cartTotal: number = 0.00;
+  cartOneTimeTotal: number = 0.00;
+  cartMonthlyTotal: number = 0.00;
+  cartSubTotal: number = 0.00;
+  cartDeliveryCosts: number = 0.00;
+
+  constructor(private productsService: ProductsService) {
+  }
+
+  ngOnInit(): void {
+    this.productsService.getProducts()
+      .then(data => this.products = data);
+  }
+
+  addToCart(product): void {
+    /* Check if the item already exists and inc. the quantity else add it*/
+    let cartItem = this.cart.find(x => x.product.id === product.id);
+
+    if (cartItem) {
+      cartItem.quantity ++;
+      cartItem.totalPrice += product.price;
+    }else {
+      cartItem = {
+        product: product,
+        quantity : 1,
+        totalPrice : product.price
+      };
+
+      this.cart.push(cartItem);
+    }
+
+    /* update Cart total */
+    this.updateTotals(cartItem);
+  }
+
+  removeFromCart(item): void {
+    let index = -1;
+    let cartItem = this.cart.find((x, i) => {
+      if (x.product.id === item.product.id) {
+        index = i;
+        return true;
+      }
+    });
+
+    this.updateTotals(cartItem, -1);
+
+    this.cart.splice(index, 1);
+  }
+
+  updateTotals(cartItem, sign = 1): void {
+    /*update subscription */
+    if (cartItem.product.subscription) {
+      this.cartMonthlyTotal += (sign * cartItem.product.price);
+    } else {
+      this.cartOneTimeTotal += (sign * cartItem.product.price);
+    }
+
+    /* update subtotal */
+    this.cartSubTotal += (sign * cartItem.product.price);
+    this.cartTotal = this.cartSubTotal + this.cartDeliveryCosts;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
 
 import { AppComponent } from './app.component';
+import { PricePipe } from './pipes/PricePipe';
+import { ProductsService } from './services/products-service.service';
 
 @NgModule({
   declarations: [
+    PricePipe,
     AppComponent
   ],
   imports: [
+    AlertModule,
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ProductsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
